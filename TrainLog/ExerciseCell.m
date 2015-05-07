656b96fb3c601e4a90b8fee66c658d6b3cab1404
Doug Voss <douglas.c.voss@gmail.com>
2015-05-06 22:40:39 -0600

    fixes background color disappearing when cell is highlighted or selected

--- TrainLog/ExerciseCell.m
+++ TrainLog/ExerciseCell.m
@@ -14,10 +14,35 @@
     // Initialization code
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated
+{
+    UIColor *backgroundColor = self.set1Reps.backgroundColor;
+    [super setHighlighted:highlighted animated:animated];
+    self.imageView.backgroundColor = backgroundColor;
+    [self resetBackgroundColors:backgroundColor];
+}
 
-    // Configure the view for the selected state
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    UIColor *backgroundColor = self.set1Reps.backgroundColor;
+    [super setSelected:selected animated:animated];
+    self.imageView.backgroundColor = backgroundColor;
+    [self resetBackgroundColors:backgroundColor];
+}
+
+- (void)resetBackgroundColors:(UIColor *)backgroundColor
+{
+    self.weightAmount.backgroundColor = backgroundColor;
+    self.set1Reps.backgroundColor = backgroundColor;
+    self.set2Reps.backgroundColor = backgroundColor;
+    self.set3Reps.backgroundColor = backgroundColor;
+    self.set4Reps.backgroundColor = backgroundColor;
+    self.set5Reps.backgroundColor = backgroundColor;
+    UIColor *warmupColor = [UIColor colorWithHue:0.2 saturation:0.7 brightness:0.9 alpha:1.0];
+    self.warmup1Amount.backgroundColor = warmupColor;
+    self.warmup2Amount.backgroundColor = warmupColor;
+    self.warmup3Amount.backgroundColor = warmupColor;
+    self.warmup4Amount.backgroundColor = warmupColor;
 }
 
 @end
